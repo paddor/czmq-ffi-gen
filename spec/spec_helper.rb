76cfbd3c7da001dd69d53c9198e9bd1427d6d695
Patrik Wenger <paddor@gmail.com>
2016-03-29 21:42:36 +0200

    remove support code for older versions of CZMQ (not supported anymore)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
 require 'bundler/setup'
 require 'rspec'
-require_relative 'czmq_helper'
 require_relative '../lib/czmq-ffi-gen'
 
 RSpec.configure do |config|
   config.expect_with :minitest
-  include CZMQHelper
 end
