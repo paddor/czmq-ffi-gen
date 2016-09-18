fac052471046a88cc6790fc881c4f12d326b46ad
Kouhei Sutou <kou@clear-code.com>
2016-09-18 22:28:14 +0900

    Extract vendor/ directory related code into a new file

--- lib/czmq-ffi-gen.rb
+++ lib/czmq-ffi-gen.rb
@@ -1,9 +1,4 @@
-base_dir = File.expand_path(File.join(__dir__, ".."))
-vendor_bin_dir = File.join(base_dir, "vendor", "local", "bin")
-if File.exist?(vendor_bin_dir)
-  ENV["PATH"] = [vendor_bin_dir, ENV["PATH"]].join(File::PATH_SEPARATOR)
-end
-
+require_relative "czmq-ffi-gen/vendor"
 require_relative "czmq-ffi-gen/czmq/ffi"
 require_relative "czmq-ffi-gen/versions"
 require_relative "czmq-ffi-gen/errors"
