30ec6a3a87f23e709bcb405f01ddf36474074476
Patrik Wenger <paddor@gmail.com>
2017-01-08 12:47:14 +0100

    skip DRAFT API specs unless explicitly wanted

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,11 @@ require_relative '../lib/czmq-ffi-gen'
 
 RSpec.configure do |config|
   config.expect_with :minitest
+
+  if ENV['DRAFT_API'] == 'yes'
+    warn 'Including specs for DRAFT API.'
+  else
+    warn 'Excluding specs for DRAFT API. To enable, set DRAFT_API=yes in the environment.'
+    config.filter_run_excluding state: :draft
+  end
 end
