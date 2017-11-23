d3e2c289b30b1aaf0d0880f0fe3988583284e037
Christian Boos <cboos@edgewall.org>
2017-11-23 16:52:21 +0100

    Add support for Ruby 2.4 on Windows (from oneclick/rubyinstaller2) (#9)
    In order to be able to load vendor dlls in this distribution of Ruby,
    adding the vendor/local/bin directory to the PATH is no longer working,
    one has to use `RubyInstaller.add_dll_directory` explicitly.
    
    See details in:
    
    https://github.com/oneclick/rubyinstaller2/wiki/For-gem-developers#dll-loading

--- lib/czmq-ffi-gen/vendor.rb
+++ lib/czmq-ffi-gen/vendor.rb
@@ -1,5 +1,13 @@
 base_dir = File.expand_path(File.join(__dir__, "..", ".."))
 vendor_bin_dir = File.join(base_dir, "vendor", "local", "bin")
 if File.exist?(vendor_bin_dir)
-  ENV["PATH"] = [vendor_bin_dir, ENV["PATH"]].join(File::PATH_SEPARATOR)
+  # On Windows, RubyInstaller2 uses kernel32's SetDefaultDllDirectories
+  # and AddDllDirectory, which disables the lookup of DLLs in the PATH, see
+  # https://github.com/oneclick/rubyinstaller2/wiki/For-gem-developers#dll-loading
+  begin
+    require 'ruby_installer'
+    RubyInstaller::Runtime.add_dll_directory vendor_bin_dir
+  rescue LoadError
+    ENV["PATH"] = [vendor_bin_dir, ENV["PATH"]].join(File::PATH_SEPARATOR)
+  end
 end
