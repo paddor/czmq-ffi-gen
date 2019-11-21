b79129ccdb946bc40ac6ed2f76213e0e7a7f566e
Patrik Wenger <patrik.wenger@mindclue.ch>
2019-11-21 16:40:15 +0100

    allow focusing on test blocks/cases

--- czmq-ffi-gen.gemspec
+++ czmq-ffi-gen.gemspec
@@ -31,5 +31,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
-  spec.add_development_dependency "minitest"
+  spec.add_development_dependency "minitest" # assertions
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'rspec'
 require_relative '../lib/czmq-ffi-gen'
 
 RSpec.configure do |config|
-  config.expect_with :minitest
+  config.expect_with :rspec, :minitest
 
   if ENV['DRAFT_API'] == 'yes'
     warn 'Including specs for DRAFT API.'
@@ -11,6 +11,9 @@ RSpec.configure do |config|
     warn 'Excluding specs for DRAFT API. To enable, set DRAFT_API=yes in the environment.'
     config.filter_run_excluding state: :draft
   end
+
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
 
 puts "Using CZMQ version #{CZMQ::FFI::CZMQ_VERSION} and ZMQ version #{CZMQ::FFI::ZMQ_VERSION}."
