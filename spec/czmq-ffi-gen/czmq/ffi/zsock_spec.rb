00cfad1716f2f8ddbd97b9d92ed98d6de5df40e7
Patrik Wenger <patrik.wenger@mindclue.ch>
2025-01-03 15:56:22 +0100

    style

--- spec/czmq-ffi-gen/czmq/ffi/zsock_spec.rb
+++ spec/czmq-ffi-gen/czmq/ffi/zsock_spec.rb
@@ -5,9 +5,10 @@ describe CZMQ::FFI::Zsock do
 
   context "with a zsock" do
     i = 0
-    let(:endpoint) { "inproc://endpoint_#{i+=1}" }
-    let(:zobj) { described_class.new_pair(endpoint) }
+    let(:endpoint)    { "inproc://endpoint_#{i+=1}" }
+    let(:zobj)        { described_class.new_pair(endpoint) }
     let(:use_pointer) { zobj.endpoint }
+
     include_examples "a zobj"
 
     it "creates REP Zsock" do
